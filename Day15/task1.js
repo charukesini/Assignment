cc434c75850689f5e1899a883ebe056404a44c6d
charukesini <144435425+charukesini@users.noreply.github.com>
2026-01-14 17:53:46 +0530

    Day15

---
+++ Day15/task1.js
@@ -0,0 +1,21 @@
+
+        // Print your name
+    console.log("Charukesini");
+
+        // COMMENT LINE
+
+
+//   This is a single-line comment example  
+    /*
+      This comment
+      spans multiple lines
+    */
+
+    // ADDITION
+
+    let a=20;
+    let b=30;
+    let z= a+b;
+
+    console.log(z);
+
