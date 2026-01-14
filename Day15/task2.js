42f61a26613c9104ce5a7f7cfae013914704ae91
charukesini <144435425+charukesini@users.noreply.github.com>
2026-01-14 17:52:39 +0530

    Day15

---
+++ Day15/task2.js
@@ -0,0 +1,14 @@
+// string data type
+console.log("Hello"); 
+
+// number data type
+console.log(100);
+
+// adding two numbers
+console.log(20 + 30);
+
+// boolean data type
+console.log(true);
+
+// printing mixed data
+console.log("Total:", 50);
